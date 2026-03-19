5c2a701183d55fefc39659bbc746aac0a818c35f
ZariasKobold <halcyon.sentinel@gmail.com>
2026-03-19 00:22:26 -0600

    Add files via upload
    Update to admin profile allowing pro to be granted by email search.

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Version is injected from APP_VERSION in index.html via cache name
 // Changing CACHE_VERSION busts the cache and triggers an update for all users
 
-const CACHE_VERSION = 'heardtale-2.0.3';
+const CACHE_VERSION = 'heardtale-2.0.4';
 const SHELL_ASSETS = [
   '/',
   '/index.html',
